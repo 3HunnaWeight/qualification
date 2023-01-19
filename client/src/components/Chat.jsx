d9fe71d7faa65f9ddcc712fa2dca2c60ea2dbff8
3HunnaWeight <vip.andrew24@mail.ru>
2023-01-19 23:11:22 +0300

    add

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import "../styles/chat.css"
 
 import { useState, useRef } from 'react';
+import { useEffect } from 'react';
 export const Chat = () => {
   const [messages, setMessages] = useState([]);
   const [value, setValue] = useState('');
@@ -43,44 +44,47 @@ export const Chat = () => {
       socket.current.send(JSON.stringify(message));
       setValue('')
   }
+ console.log(messages)
 
 
   if (!connected) {
       return (
+        <div className="chat" style={{marginTop:"1%"}}>
           <div className="center">
               <div className="form">
-                  <input
+                  <input className='input'
                       value={username}
                       onChange={e => setUsername(e.target.value)}
                       type="text"
                       placeholder="Введите ваше имя"/>
-                  <button onClick={connect}>Войти</button>
+                  <button className='buttonConnect' onClick={connect}>Войти</button>
               </div>
           </div>
+        </div>
       )
   }
 
 
   return (
-      <div className="chat" style={{margin:"1%"}}>
+      <div className="chat" style={{marginTop:"1%"}}>
           
               <div className="form">
                   <input className='input' placeholder='Сообщение' value={value} onChange={e => setValue(e.target.value)} type="text"/>
                   <div className='btnSend' onClick={sendMessage}></div>
               </div>
-              <div className="messages">
-                  {messages.map(mess =>
-                      <div key={mess.id}>
-                          {mess.event === 'connection'
-                              ? <div className="connection_message">
-                                  Пользователь {mess.username} подключился
-                              </div>
-                              : <div className="message">
-                                  {mess.username}. {mess.message}
-                              </div>
-                          }
-                      </div>
-                  )}
+              <div className='containerMessages'>
+                    <div className="messages">
+                        {messages.map(mess =>
+                            <div key={mess.id}>
+                                {mess.event === 'connection'
+                                    ? console.log(mess.username)
+                                    : <div className="message">
+                                        {mess.username}: {mess.message}
+                                    </div>
+                                }
+                            </div>
+                        )}
+                    </div>
               </div>
           </div>
       
